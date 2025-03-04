84d516a986cb6fc78d9bcc2d5e699e7392c4d40a
Gabriel Lulu <glulu4444@gmail.com>
2025-03-04 15:17:27 -0500

    name

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 const buildConfig = () => {
 
-  const name = process.env.NEXT_PUBLIC_DISPLAY_NAME || "Display Name.";
+  const name = process.env.NEXT_PUBLIC_DISPLAY_NAME || "Gabby Lulu";
   const copyright = process.env.NEXT_PUBLIC_COPYRIGHT || "Copyright";
   const defaultTitle =
-    process.env.NEXT_DEFAULT_METADATA_DEFAULT_TITLE || "Meta title";
-  const defaultDescription = process.env.NEXT_PUBLIC_DESCRIPTION || "Description";
+    process.env.NEXT_DEFAULT_METADATA_DEFAULT_TITLE || "Gabby Lulu";
+  const defaultDescription = process.env.NEXT_PUBLIC_DESCRIPTION || "Gabby Lulu's personal website";
 
   return {
     baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
