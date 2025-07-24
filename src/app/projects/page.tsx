b3e029e1933244dd32556c5f60af7011a41ad7cc
Gabriel Lulu <glulu4444@gmail.com>
2025-07-24 13:23:47 -0400

    edist

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,260 +1,3 @@
-// // "use client";
-// // import React, {useState} from "react";
-// // import ProjectHero from "@/components/ProjectHero";
-// // import WorkRow from "@/components/WorkRow";
-// // import {Category, ProjectData, projects} from "../../../public/projects";
-// // import clsx from "clsx";
-// // import Links from "@/components/Links";
-// // import {Loader2} from "lucide-react";
-// // import {PillButton} from "@/components/PillButton";
-
-// // const Page = () => {
-// //     const [selectedProject, setSelectedProject] = useState<ProjectData | null>();
-// //     const [isTransitioning, setIsTransitioning] = useState(false);
-// //     const [videoLoaded, setVideoLoaded] = useState(false);
-
-
-// //     const handleProjectClick = (project: ProjectData) => {
-// //         setIsTransitioning(true); // Start fade-out
-
-// //         setTimeout(() => {
-// //             setSelectedProject(project); // Switch to project details
-// //             setIsTransitioning(false); // Fade-in project
-// //         }, 500); // Match transition duration
-// //     };
-
-// //     const handleBackClick = () => {
-// //         setIsTransitioning(true); // Start fade-out
-// //         setTimeout(() => {
-// //             setSelectedProject(null); // Show project list again
-// //             setIsTransitioning(false); // Fade-in list
-// //         }, 500);
-// //     };
-
-// //     function renderProjectDetails(project: ProjectData) {
-        
-// //         if (!project) 
-// //             return;
-
-        
-// //         return (
-// //             <div className="transition-opacity duration-500 opacity-100">
-// //                 <button onClick={handleBackClick} className="text-hoverColor text-sm sm:text-xl hover:text-hoverColor mb-4 ">
-// //                     ← Back
-// //                 </button>
-// //                 <h1 className="text-6xl sm:text-7xl font-bold text-whitesmoke">{project.title}</h1>
-
-
-// //                 <div className="mt-10 w-5/6">
-// //                     <p className="text-lg sm:text-xl font-medium sm:leading-10 text-whitesmoke">{project.description}</p>
-// //                 </div>
-
-
-// //                 {project.link && (
-// //                     <div className="mt-5">
-// //                         <a
-// //                             href={project.link}
-// //                             target="_blank"
-// //                             rel="noreferrer"
-// //                             className="text-lg sm:text-xl font-medium text-hoverColor hover:text-hoverColor "
-// //                         >
-// //                             View Project
-// //                         </a>
-// //                     </div>
- 
-// //                 )}
-// //             </div>
-// //         )
-// //     }
-
-// //     const filterOptions:Category[] = Object.values(Category).map((category) => category);
-
-
-
-
-// //     return (
-// //         <div>
-// //             <ProjectHero 
-// //             onVideoReady={() => setVideoLoaded(true)}
-// //             video="/videos/tennis.mp4" 
-// //             header={
-// //                 !selectedProject ? (
-// //                     <div
-// //                         className={`flex flex-col items-start space-y-4 transition-opacity duration-500 ${isTransitioning ? "opacity-0" : "opacity-100"}`}>
-// //                         <h1 className="text-6xl sm:text-8xl font-bold text-textSec">
-// //                             Work
-// //                         </h1>
-// //                     <Links/>
-// //                     </div>
-// //                 ) : (   
-// //                     <></>
-// //                 )
-// //             }>
-// //                 {!videoLoaded ? (
-// //                     <div className="absolute inset-0 flex items-center justify-center">
-// //                         <Loader2 className="h-16 w-16 animate-spin text-white" />
-// //                     </div>
-// //                 ) : 
-// //                 (
-// //                     <div>
-
-// //                         <PillButton
-// //                         filterOptions={filterOptions}
-// //                         />
-// //                             <div
-// //                                 className={
-// //                                     clsx(`flex flex-col space-y-8 w-full max-w-5xl mx-auto h-[70vh] py-4 relative transition-opacity duration-500 ${isTransitioning ? "opacity-0" : "opacity-100"}`,
-// //                                         "overflow-scroll overflow-x-visible"
-// //                                     )}
-// //                             >
-// //                                 {!selectedProject ? (
-// //                                     // Project List View
-// //                                     projects.map((item, index) => (
-// //                                         <div key={index} onClick={() => handleProjectClick(item)}>
-// //                                             <WorkRow
-// //                                                 title={item.title}
-// //                                                 skills={item.skills}
-// //                                                 category={item.category}
-// //                                             />
-// //                                         </div>
-// //                                     ))
-// //                                 ) : (
-// //                                     // Project Details View
-// //                                     renderProjectDetails(selectedProject)
-// //                                 )}
-// //                             </div>
-// //                     </div>
-
-// //                 )}
-
-// //             </ProjectHero>
-// //         </div>
-// //     );
-// // };
-
-// // export default Page;
-// "use client";
-// import React, {useState} from "react";
-// import ProjectHero from "@/components/ProjectHero";
-// import WorkRow from "@/components/WorkRow";
-// import {Category, ProjectData} from "../../../public/projects";
-// import clsx from "clsx";
-// import Links from "@/components/Links";
-// import {Loader2} from "lucide-react";
-// import {PillButton} from "@/components/PillButton";
-// import {useProjects} from "../hooks/useProjects";
-
-// const Page = () => {
-//     const [videoLoaded, setVideoLoaded] = useState(false);
-
-//     const {
-//         selectedProject,
-//         isTransitioning,
-//         selectedCategory,
-//         handleProjectClick,
-//         handleBackClick,
-//         handleCategoryFilter,
-//         clearFilter,
-//         filteredProjects,
-//     } = useProjects();
-
-//     function renderProjectDetails(project: ProjectData) {
-//         if (!project)
-//             return;
-
-//         return (
-//             <div className="transition-opacity duration-500 opacity-100">
-//                 <button onClick={handleBackClick} className="text-hoverColor text-sm sm:text-xl hover:text-hoverColor mb-4 ">
-//                     ← Back
-//                 </button>
-//                 <h1 className="text-6xl sm:text-7xl font-bold text-whitesmoke">{project.title}</h1>
-
-//                 <div className="mt-10 w-5/6">
-//                     <p className="text-lg sm:text-xl font-medium sm:leading-10 text-whitesmoke">{project.description}</p>
-//                 </div>
-
-//                 {project.link && (
-//                     <div className="mt-5">
-//                         <a
-//                             href={project.link}
-//                             target="_blank"
-//                             rel="noreferrer"
-//                             className="text-lg sm:text-xl font-medium text-hoverColor hover:text-hoverColor "
-//                         >
-//                             View Project
-//                         </a>
-//                     </div>
-//                 )}
-//             </div>
-//         )
-//     }
-
-//     const filterOptions: Category[] = Object.values(Category).map((category) => category);
-
-//     return (
-//         <div>
-//             <ProjectHero
-//                 onVideoReady={() => setVideoLoaded(true)}
-//                 video="/videos/tennis.mp4"
-//                 header={
-//                     !selectedProject ? (
-//                         <div
-//                             className={`flex flex-col items-start space-y-4 transition-opacity duration-500 ${isTransitioning ? "opacity-0" : "opacity-100"}`}>
-//                             <h1 className="text-6xl sm:text-8xl font-bold text-textSec">
-//                                 Work
-//                             </h1>
-//                             <Links />
-//                         </div>
-//                     ) : (
-//                         <></>
-//                     )
-//                 }>
-//                 {!videoLoaded ? (
-//                     <div className="absolute inset-0 flex items-center justify-center">
-//                         <Loader2 className="h-16 w-16 animate-spin text-white" />
-//                     </div>
-//                 ) :
-//                     (
-//                         <div>
-//                             <PillButton
-//                                 filterOptions={filterOptions}
-//                                 onCategorySelect={handleCategoryFilter}
-//                                 selectedCategory={selectedCategory}
-//                                 onClearFilter={clearFilter}
-//                             />
-//                             <div
-//                                 className={
-//                                     clsx(`flex flex-col space-y-8 w-full max-w-5xl mx-auto h-[70vh] py-4 relative transition-opacity duration-500 ${isTransitioning ? "opacity-0" : "opacity-100"}`,
-//                                         "overflow-scroll overflow-x-visible"
-//                                     )}
-//                             >
-//                                 {!selectedProject ? (
-//                                     // Project List View
-//                                     filteredProjects.map((item: ProjectData, index:number) => (
-//                                         <div key={index} onClick={() => handleProjectClick(item)}>
-//                                             <WorkRow
-//                                                 title={item.title}
-//                                                 skills={item.skills}
-//                                                 category={item.category}
-//                                             />
-//                                         </div>
-//                                     ))
-//                                 ) : (
-//                                     // Project Details View
-//                                     renderProjectDetails(selectedProject)
-//                                 )}
-//                             </div>
-//                         </div>
-//                     )}
-//             </ProjectHero>
-//         </div>
-//     );
-// };
-
-// export default Page;
-
-
-
 "use client";
 import React, {useEffect, useState} from "react";
 import ProjectHero from "@/components/ProjectHero";
@@ -276,7 +19,7 @@ const Page = () => {
 
     useEffect(() => {
         const handleResize = () => {
-            setWidth(window.innerWidth * 0.7);
+            setWidth(window.innerWidth * 0.8);
         };
 
         handleResize(); // Set initial width
@@ -307,9 +50,9 @@ const Page = () => {
                 <button onClick={handleBackClick} className="text-hoverColor text-sm sm:text-xl hover:text-hoverColor mb-4 ">
                     ← Back
                 </button>
-                <h1 className="text-6xl sm:text-7xl font-bold text-whitesmoke">{project.title}</h1>
+                <h1 className="text-4xl w-full sm:text-7xl font-bold text-whitesmoke">{project.title}</h1>
 
-                <div className="mt-10 w-5/6">
+                <div className="mt-10 sm:w-5/6">
                     <p className="text-lg sm:text-xl font-medium sm:leading-10 text-whitesmoke">{project.description}</p>
                 </div>
 
@@ -368,7 +111,7 @@ const Page = () => {
                             <div
                             
                                 className={
-                                    clsx(`flex flex-col space-y-8 max-w-5xl mx-auto h-[70vh] py-4 relative transition-opacity duration-500 ${isTransitioning ? "opacity-0" : "opacity-100"}`,
+                                    clsx(`flex flex-col space-y-8 sm:max-w-7xl mx-auto h-[70vh] py-4 relative transition-opacity duration-500 ${isTransitioning ? "opacity-0" : "opacity-100"}`,
                                         "overflow-scroll overflow-x-visible"
                                     )}
                             >
